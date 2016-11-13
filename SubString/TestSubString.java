c0ea3fc2d5ff87aec8eb7d08012e7012630ed929
Caetano <caetano.xu@yahoo.com>
2016-11-13 17:37:46 +0800

    20161113173700

---
+++ SubString/TestSubString.java
@@ -0,0 +1,16 @@
+public class TestSubString
+{
+	public static void main(String[] args) 
+	{
+		String str = "ABCDEFG";
+		
+		String str1 = str.substring(2, 4);
+		System.out.println(str1);
+
+		String str2 = str.substring(1,2);
+		System.out.println(str2);
+
+		String str3 = str.substring(5,6);
+		System.out.println(str3);
+	}
+}

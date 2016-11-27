2b6416fb2dd1acc46ac39bb5890ecd896578f71f
Caetano <caetano.xu@yahoo.com>
2016-11-27 20:25:46 +0800

    20161127202500

--- TestInterface.java
+++ TestInterface.java
@@ -1,33 +1,49 @@
-public class TestInterface
+public class TestInterface 
 {
 	public static void main(String[] args) 
 	{
-		Test test = new Test();
-		test.fun1();
-		test.fun2();
+		E e = new E();
+		e.fun1();
 	}
 }
 
 interface A
 {
-	public abstract void fun1();
+	public void fun2();
 }
 
-interface B
+interface B extends A
 {
-	public abstract void fun2();
+	public void fun1();
 }
 
-class Test implements A,B
+class C
+{
+	public C()
+	{
+	
+	}
+}
+
+abstract class D extends C implements A
 {
 	public void fun1()
 	{
-		System.out.println("void fun1()");
+		System.out.println("fun1()");
 	}
 
 	public void fun2()
 	{
-		System.out.println("void fun2()");
+		System.out.println("fun2()");
 	}
+
+	public abstract void fun3();
 }
 
+class E extends D
+{
+	public void fun3()
+	{
+		System.out.println("fun3()");
+	}
+}
---
+++ TestRound.java
@@ -0,0 +1,12 @@
+public class TestRound
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(Math.round(11.5));
+		System.out.println(Math.round(-11.5));
+		System.out.println(Math.round(-11.0));
+		System.out.println(Math.round(11.0));
+		System.out.println(Math.round(-11.1));
+		System.out.println(Math.round(11.1));
+	}
+}
